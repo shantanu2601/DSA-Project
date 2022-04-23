1ee3b015aa7c80701761fd3a1e9f81a9e94fb751
Revanth Reddy <90049622+revanthbellanagari@users.noreply.github.com>
2022-04-23 16:56:38 +0530

    Update classes.cpp

--- classes.cpp
+++ classes.cpp
@@ -7,90 +7,89 @@
 
 using namespace std;
 
-class messager
-{
-		user *start;		//start pointer of user DLL
-		user *last;			//pointer to last node of user DLL
-	public:
-		messager()
-		{
-			start = NULL;
-			last = NULL;
-		}
-
-		bool is_empty();			//returns true if no user has created account yet
-		user* accept();				//takes input required while creating a new account
-		void create(); 				//creates new user account & adds it to user DLL(sign-up)
-		void login();  				//to login to an existing account
-		void remove(); 				//to delete your account
-		void change_pw();			//to change current password
-		void activity(user *ptr); 		//actions that user can perform while logged in
-		msg* msg_sent(); 				//takes input to send msg, updates receiver's inbox & returns pointer to sent msg
-		void send_msg(user *ptr); 		//calls msg_sent() & updates user's sent msg sll
-};
-
 class msg
 {
 	public:
-		bool star;			//true if msg is starred
+		bool msgstar;			//true if msg is starred
 		bool sent;			//true if msg has been sent to whom the user wishes to
 		bool read;			//true if msg has been read by the logged-in user
-		string dt; 			//date & time when msg was sent/received
-		string to;			//username of user to whom msg is sent
-		string from;		        //username of user from whom msg is sent
+		string dattime; 			//date & time when msg was sent/received
+		string userto;			//username of user to whom msg is sent
+		string userfrom;		        //username of user from whom msg is sent
 		string text;		        //the actual message
 		msg *link;
 
 		msg()
 		{
-			star = false;
+			msgstar = false;
 			sent = false;
 			read = true;
-			to = "";
-			from = "";
+			userto = "";
+			userfrom = "";
 			text = "";
 			link = NULL;
 		}
 };
 
+//node for user DLL
 class user
 {
 	public:
-		bool logged_in;
+		bool loggedin;
 		string username;
 		string password;
-		msg *headS; 			//sent msg SLL head. so for each user we have a (single)linked list of sent messages
-		msg *headR; 			//received msg SLL head. so for each user we also have a (single)linked list of received messages
+		msg *head2; 			//Singly Linked List for sent messages. so for each user we have a (single)linked list of sent messages
+		msg *head1; 			//Singly linked List for reciever messages. so for each user we also have a (single)linked list of received messages
 		vector<msg*> trash;		//vector of deleted msg
 		user *next;
 		user *prev;
 		//friend class messager;
 		user()
 		{
-			logged_in = false;
+			loggedin = false;
 			username = "";
 			password = "";
-			headS = NULL;
-			headR = NULL;
+			head2 = NULL;
+			head1 = NULL;
 			next = NULL;
 			prev = NULL;
 		}
 
-		void display_msgs(string title, msg *head);				//to display list of sent/inbox msg
-		void msg_options(string title, msg **head);				//actions user can perform with displayed list of msg
-		void read_msg(msg *head);					        //to read a certain msg
-		void del_msg(msg **head);						//to delete a certain msg
-		void starUnstar_msg(msg *m);						//to mark an msg as important (star) or unstar
-		void vec_read_msg(vector<msg*> results);				//to read msg from search results
-		void vec_del_msg(vector<msg*> results, msg **head); 	                //to delete msg from search results
+		void displaymsg(string title, msg *head);				//to display list of sent/inbox msg
+		void useroptions(string title, msg **head);				//actions user can perform with displayed list of msg
+		void msgread(msg *head);					        //to read a certain msg
+		void msgdel(msg **head);						//to delete a certain msg
+		void starunstarr(msg *m);						//to mark an msg as important (star) or unstar
+		void vec_msgread(vector<msg*> results);				//to read msg from search results
+		void vec_msgdel(vector<msg*> results, msg **head); 	                //to delete msg from search results
 		void vec_starUnstar(vector<msg*> results); 				//to star/unstar msg from search results
-		void search_msg(string title, msg **head);				//to search msg sent to/ received from a user
-		void starred_msg(string title, msg **head);				//displays list of starred msg
-		void trash_options();							//actions to perform on deleted msg
-		void del_permanently();							//to delete a msg from trash (permanently)
-		void read_trashMsg();							//to read a msg in trash
+		void msgsearch(string title, msg **head);				//to search msg sent to/ received from a user
+		void strmsg(string title, msg **head);				//displays list of starred msg
+		void deloptions();							//actions to perform on deleted msg
+		void permdelete();							//to delete a msg from trash (permanently)
+		void trashread();							//to read a msg in trash
 
 };
 
+class mailingclone
+{
+		user *first;		        //start pointer of user DLL
+		user *end;			//pointer to last node of user DLL
+	public:
+		mailingclone()
+		{
+			first = NULL;
+			end = NULL;
+		}
 
+		bool noaccount();			//returns true if no user has created account yet
+		user* inputacc();				//takes input required while creating a new account
+		void acclogin();  				//to login to an existing account
+		void delacc(); 				//to delete your account
+		void pswchange();			//to change current password
+		void useractions(user *n); 		//actions that user can perform while logged in
+		void newaccount(); 				//creates new user account & adds it to user DLL(sign-up)
+		msg* sendmessage(); 				//takes input to send msg, updates receiver's inbox & returns pointer to sent msg
+		void sendmsgfunc(user *n); 		//calls msg_sent() & updates user's sent msg sll
+};
 
